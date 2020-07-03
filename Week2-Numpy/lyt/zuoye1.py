4ced1d7119d94a4a7c60ac77c6904cadd83a3dbe
Liang YiTing <799300435@qq.com>
2020-07-03 15:57:24 +0800

    梁

--- Week2-Numpy/lyt/zuoye1.py
+++ Week2-Numpy/lyt/zuoye1.py
@@ -31,47 +31,70 @@ A=a*10;A
 ##  4. 随机生成8乘以20的整数array，并得到其转置数组，对其转置数组提取前10行，第2,5,8列数据
 
 import numpy as np 
-r=np.random.randint(1,100,size=160)
-R=r.reshape((8,20),order='c');R
+R=np.random.randint(1,100,size=(8,20));R
 
 RT=R.T;RT     #转置
 
-import pandas as pd
-rt1=RT[:10,1]  #第2列数据
-rt2=RT[:10,4]  #第5列数据
-rt3=RT[:10,7]  #第8列数据
-rt_1=pd.Series(rt1)
-rt_2=pd.Series(rt2)
-rt_3=pd.Series(rt3)
-rtc=pd.concat([rt_1,rt_2,rt_3],axis=1);rtc
+RT[:10,[1,4,7]]
 
 
 ##  5. 利用pandas把data文件夹中的 数据.xls 导入，要求使用相对路径
 
-
-
+import pandas as pd 
+data=pd.read_excel('../data/数据.xls',0)
 
 
 ##  6. 显示 数据.xls 数据集的结构，前5条数据，后5条数据，所有变量名，及其维度
 
+data.info()    #数据集结构
+data.head()    #前5条数据
+data.tail()    #后5条数据
+data.columns   #所有变量名
+data.shape     #数据集维度
+
+
 ##  7. 对数据集增加一个变量HRsq，其为 HR数据的平方
+
+data['HRsq']=data.HR**2
+
 
 ##  8. 对 数据.xls 数据集 进行子集的提取：
 
 ###     1. 删除有缺失值的年份
 
+d1=data.dropna();d1
+
 ###     2. 提取逢5，逢0年份的Year, GDP, KR, HRsq，CPI 变量的数据
+
+d2=data.loc[[1,6,11,16,21,26,31,36,41,46,51,56,61],['Year','GDP','KR','HRsq','CPI']];d2
 
 ###     3. 提取逢2，逢8年份的Year, GDP, KR, HRsq，CPI 变量的数据
 
+d3=data.loc[[3,9,13,19,23,29,33,39,43,49,53,59],['Year','GDP','KR','HRsq','CPI']];d3
+
 ###     4. 对2和3得到的数据集按行进行合并，并按年份进行排序  1111 
+
+import pandas as pd 
+d4=pd.concat([d2,d3],axis=0)
+D4=d4.sort_values(by='Year');D4
 
 ###     5. 提取1978年之后的数据
 
+d5=data[data.Year>1978];d5
+
 ###     6. 提取1978年之后且 KR 变量在 1~1.2之间的数据
+
+d6=data[(data.Year>1978)&(data.KR>1)&(data.KR<1.2)];d6
+
 
 ##  9. 保存数据为csv和excel
 
 ###     1. 写出第8题中第4问得到的子集到data文件夹
 
+D4.to_csv('../data/data1_lyt.csv')
+D4.to_excel('../data/data1_lyt.xlsx')
+
 ###     2. 写出第8题中第6问得到的子集到data文件夹
+
+d6.to_csv('../data/data2_lyt.csv')
+d6.to_excel('../data/data2_lyt.xlsx')
